3fa99f5c1220d146db771ce1b4fcee6c1f8a6f35
Lani Southgate <lani.bateman@gmail.com>
2020-05-11 22:30:36 -0600

    Remove references to motion

--- motion-onvif-events.js
+++ motion-onvif-events.js
@@ -5,40 +5,20 @@ const fetch = require('node-fetch');
 const {ArgumentParser} = require('argparse');
 const pjson = require('./package.json');
 
-class MotionService {
-  constructor(basePath) {
-    this.basePath = basePath
-  }
-
-  /**
-   * @param {number} cameraId
-   * @param {boolean} state
-   */
-  setAlarm(cameraId, state) {
-    const date = new Date().toLocaleString();
-    console.log(`[${date}] Setting camera ${cameraId} to state ${state}`);
-    const cmd = state ? 'on' : 'off';
-    const url = `${this.basePath}${cameraId}/config/set?emulate_motion=${cmd}`;
-    return fetch(url);
-  }
-}
-
 let MotionTopic = {
   CELL_MOTION_DETECTOR: 'CELL_MOTION_DETECTOR',
   MOTION_ALARM: 'MOTION_ALARM',
 };
 
 class Monitor {
-  constructor(id, onvifCam, motion) {
-    this.id = id;
+  constructor(onvifCam) {
     this.onvifCam = onvifCam;
-    this.motion = motion;
     this.lastMotionDetectedState = null;
     this.topic = MotionTopic.MOTION_ALARM;
   }
 
   log(msg, ...rest) {
-    console.log(`[camera ${this.id}]: ${msg}`, ...rest);
+    console.log(`[camera]: ${msg}`, ...rest);
   }
 
   async start() {
@@ -57,7 +37,6 @@ class Monitor {
     const isMotion = camMessage.message.message.data.simpleItem.$.Value;
     if (this.lastMotionDetectedState !== isMotion) {
       this.log(`CellMotionDetector: Motion Detected: ${isMotion}`);
-      this.motion.setAlarm(this.id, isMotion);
     }
     this.lastMotionDetectedState = isMotion
   }
@@ -68,43 +47,32 @@ class Monitor {
     })
   }
 
-  static async create({id, hostname, username, password, port}, motion) {
+  static async create({hostname, username, password, port}) {
     const cam = await this.createCamera({
       hostname,
       username,
       password,
       port
     });
-    return new Monitor(id, cam, motion);
+    return new Monitor(cam);
   }
 }
 
 async function start(args) {
-  const motion = new MotionService(args.motion_base_url);
   const monitor = await Monitor.create({
-    id: args.motion_camera_id,
     hostname: args.hostname,
     username: args.username,
     password: args.password,
     port: args.port
-  }, motion);
+      });
   monitor.start();
 }
 
 function main() {
   const parser = new ArgumentParser({
     addHelp: true,
-    description: 'ONVIF motion detection events bridge to Motion',
+    description: 'ONVIF motion detection events',
     version: pjson.version,
-  });
-
-  parser.addArgument(['-m', '--motion-base-url'], {
-    help: 'Base URL for the Motion instance (with trailing slash)',
-    required: true
-  });
-  parser.addArgument(['-i', '--motion-camera-id'], {
-    help: 'The ID of the camera in Motion',
-    required: true
   });
   parser.addArgument(['-c', '--hostname'], {
     help: 'hostname/IP of the ONVIF camera',
